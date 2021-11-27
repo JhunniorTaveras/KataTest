13ae2a82d57876b728ae34114570f52b3d2fcaa3
Jhunnior Taveras <66530696+JhunniorTaveras@users.noreply.github.com>
2021-11-26 20:53:24 -0400

    Add files via upload

---
+++ ISBNTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataISBN;
+using System;
+
+namespace KataISBN.UnitTests
+{
+    [TestClass]
+    public class ISBNTests
+    {
+        [TestMethod]
+        public void CodigoDiezTest_ReturnTrue()
+        {
+            var codigo = new ISBN();
+
+            var result = codigo.CodigoDiez(new String);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CodigoDiezTest_ReturnFalse()
+        {
+            var codigo = new ISBN();
+
+            var result = codigo.CodigoDiez(new String);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CodigoTreceTest_ReturnTrue()
+        {
+            var codigo = new ISBN();
+
+            var result = codigo.CodigoTrece(new String);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CodigoTreceTest_ReturnFalse()
+        {
+            var codigo = new ISBN();
+
+            var result = codigo.CodigoTrece(new String);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
